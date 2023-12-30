352e8df8af9fb3b7a2678716b8142d94742b5a63
Moein Madadi <moeinmadadi2002@gmail.com>
2023-12-30 16:42:07 +0330

    add files

---
+++ lib/plot_histogram.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_in_out_histogram(hist_name, id_list_name, id_list, out_list_name, out_list):
+
+    # Plot histograms
+    plt.hist(id_list, bins=30, alpha=0.5, color='blue', label=id_list_name)
+    plt.hist(out_list, bins=30, alpha=0.5, color='orange', label=out_list_name)
+
+    # Add labels and title
+    plt.xlabel('Values')
+    plt.ylabel('Frequency')
+    plt.title(f'Histogram of ID VS OUT {hist_name}')
+
+    # Add legend
+    plt.legend()
+
+    # Show the plot
+    plt.show()
