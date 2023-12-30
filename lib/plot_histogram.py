0f75a10eec77e19ef0c16754f362541afacf1298
Moein Madadi <moeinmadadi2002@gmail.com>
2023-12-30 20:25:01 +0330

    change model loading

--- lib/plot_histogram.py
+++ lib/plot_histogram.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
 def plot_in_out_histogram(hist_name, id_list_name, id_list, out_list_name, out_list):
-
     # Plot histograms
-    plt.hist(id_list, bins=30, alpha=0.5, color='blue', label=id_list_name)
-    plt.hist(out_list, bins=30, alpha=0.5, color='orange', label=out_list_name)
+    plt.hist(id_list, bins=30, alpha=0.5, color='blue', label=id_list_name.cpu().detach().numpy())
+    plt.hist(out_list, bins=30, alpha=0.5, color='orange', label=out_list_name.cpu().detach().numpy())
 
     # Add labels and title
     plt.xlabel('Values')
