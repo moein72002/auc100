13996d725376b15cdbff875e25aca9de4a24daf0
Moein Madadi <moeinmadadi2002@gmail.com>
2023-12-31 17:40:08 +0330

    fix

--- lib/plot_histogram.py
+++ lib/plot_histogram.py
@@ -4,8 +4,9 @@ def plot_in_out_histogram(hist_name, id_list_name, id_list, out_list_name, out_l
     print("start plot_in_out_histogram")
     print(f"{id_list_name}: {id_list}")
     print(f"{id_list_name} type: {type(id_list)}")
+    print(f"{id_list_name} shape: {id_list.shape}")
     print(f"{out_list_name}: {out_list}")
-    print(f"{out_list_name} type: {type(out_list)}")
+    print(f"{out_list_name} shape: {out_list.shape}")
     # Plot histograms
     plt.hist(id_list, bins=100, alpha=0.5, color='blue', label=id_list_name)
     plt.hist(out_list, bins=100, alpha=0.5, color='orange', label=out_list_name)
